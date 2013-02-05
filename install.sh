97647d8d606d609e108d2beb083cd0918c4e9355
Adam Ashley <adama@swiftnetworks.com.au>
2013-02-05 09:41:19 +0800

    Make install easier

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 git clone https://github.com/gmarik/vundle.git bundle/vundle
+sudo apt-get install python-pip
+
 git clone git://github.com/sigurdga/gnome-terminal-colors-solarized.git
+pushd gnome-terminal-colors-solarized
+./solarize
+popd
