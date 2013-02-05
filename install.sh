e5e8083865e0f917b60f623cfe916080d30a2ba5
Adam Ashley <aashley@adamashley.name>
2013-02-05 09:48:46 +0800

    Complete install script

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 git clone https://github.com/gmarik/vundle.git bundle/vundle
 sudo apt-get install python-pip
+pip install --user git+git://github.com/Lokaltog/powerline
+vim +BundleInstall +qall
 
 git clone git://github.com/sigurdga/gnome-terminal-colors-solarized.git
 pushd gnome-terminal-colors-solarized
