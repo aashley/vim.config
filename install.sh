7b448f959a8574ccef28202629925a719f48300f
Adam Ashley <adama@swiftnetworks.com.au>
2013-02-07 13:14:07 +0800

    Add creation of undodir

--- install.sh
+++ install.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
+BASEDIR=`dirname $0`
+
 git clone https://github.com/gmarik/vundle.git bundle/vundle
 sudo apt-get install python-pip
 pip install --user git+git://github.com/Lokaltog/powerline
+
+if [ ! -h ~/.vimrc ]
+then
+    mv ~/.vimrc ~/.vimrc.old
+fi
+if [ ! -h ~/.vim ]
+then
+    mv ~/.vim ~/.vim.old
+fi
+
+ln -s $BASEDIR ~/.vim
+ln -s $BASEDIR/vimrc ~/.vimrc
+
 vim +BundleInstall +qall
 
 mkdir -p ~/.fonts/ ~/.fonts.conf.d/ ~/.config/fontconfig/conf.d/
-cp bundle/powerline/font/PowerlineSymbols.otf ~/.fonts/
+cp $BASEDIR/bundle/powerline/font/PowerlineSymbols.otf ~/.fonts/
 fc-cache -vf ~/.fonts/
-cp bundle/powerline/font/10-powerline-symbols.conf ~/.fonts.conf.d/
-cp bundle/powerline/font/10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+cp $BASEDIR/bundle/powerline/font/10-powerline-symbols.conf ~/.fonts.conf.d/
+cp $BASEDIR/bundle/powerline/font/10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
 
 git clone git://github.com/sigurdga/gnome-terminal-colors-solarized.git
-pushd gnome-terminal-colors-solarized
+pushd $BASEDIR/gnome-terminal-colors-solarized
 ./solarize
 popd
+
+mkdir -p $BASEDIR/undodir
